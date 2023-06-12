610ce53844728ab36516a832645865e732e53761
asifhossain2k20 <asif.hossain2k20@gmail.com>
2023-06-12 22:48:33 +0600

    ESLinyt Update

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app: Application = express()
+import userRouter from './app/modules/users/users.route'
 
+const app: Application = express()
 app.use(cors())
 
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+//Application Routes
+app.use('/api/v1/users/', userRouter.router)
 //Testing
-app.get('/', (req: Request, res: Response) => {
+app.get('/', async (req: Request, res: Response) => {
   res.send('Working Successfully')
 })
 
